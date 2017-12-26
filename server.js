89c0e9091545c2d63dc20cdc9ef6e231b5a19e6a
PERSONALSOFT\joaguirre <joaguirre@personalsoft.com.co>
2017-12-26 08:56:52 -0500

    challenge collection added

--- server.js
+++ server.js
@@ -15,11 +15,12 @@ server.connection({
 
 //Connect to db
 server.app.db = mongojs('language-challenge:3gOJ3E4cEI5zQslDiKrLtF3bhdVCi6ABf4E21Rs5YfX6wKDK3LzRemttmUK70W89e0F7dzx0KG3nAzBpltCk4Q==@language-challenge.documents.azure.com:10255/?ssl=true&replicaSet=globaldb', ['words','score']);
-
+// server.app.db = mongojs('localhost:27017/local', ['words','score']);
 //Load plugins and start server
 server.register([
     require('./routes/words'),
-    require('./routes/score')
+    require('./routes/score'),
+    require('./routes/challenge')
 ], (err) => {
 
     if (err) {
